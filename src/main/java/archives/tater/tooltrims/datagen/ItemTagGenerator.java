b13f6cbcebf9c6f667298ad04401bc8fcca65daa
JosiahFu <josiahfu@gmail.com>
2024-10-22 23:05:38 -0700

    populate tag manually

--- src/main/java/archives/tater/tooltrims/datagen/ItemTagGenerator.java
+++ src/main/java/archives/tater/tooltrims/datagen/ItemTagGenerator.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -18,9 +17,39 @@ public class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
         getOrCreateTagBuilder(ToolTrimsTags.TRIMMABLE_TOOLS)
-                .forceAddTag(ItemTags.TOOLS)
-                .forceAddTag(ItemTags.SWORDS)
-                .add(Items.BOW)
-                .add(Items.CROSSBOW);
+                .add(
+                        Items.WOODEN_SWORD,
+                        Items.WOODEN_SHOVEL,
+                        Items.WOODEN_PICKAXE,
+                        Items.WOODEN_AXE,
+                        Items.WOODEN_HOE,
+                        Items.STONE_SWORD,
+                        Items.STONE_SHOVEL,
+                        Items.STONE_PICKAXE,
+                        Items.STONE_AXE,
+                        Items.STONE_HOE,
+                        Items.GOLDEN_SWORD,
+                        Items.GOLDEN_SHOVEL,
+                        Items.GOLDEN_PICKAXE,
+                        Items.GOLDEN_AXE,
+                        Items.GOLDEN_HOE,
+                        Items.IRON_SWORD,
+                        Items.IRON_SHOVEL,
+                        Items.IRON_PICKAXE,
+                        Items.IRON_AXE,
+                        Items.IRON_HOE,
+                        Items.DIAMOND_SWORD,
+                        Items.DIAMOND_SHOVEL,
+                        Items.DIAMOND_PICKAXE,
+                        Items.DIAMOND_AXE,
+                        Items.DIAMOND_HOE,
+                        Items.NETHERITE_SWORD,
+                        Items.NETHERITE_SHOVEL,
+                        Items.NETHERITE_PICKAXE,
+                        Items.NETHERITE_AXE,
+                        Items.NETHERITE_HOE,
+                        Items.BOW,
+                        Items.CROSSBOW
+                );
     }
 }
