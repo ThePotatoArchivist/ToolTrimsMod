bfe4dde2bbe4f93dc99076fa11f02029b0329cd2
ThePotatoArchivist <josiahfu@gmail.com>
2025-04-29 12:36:13 -0700

    Fix loot amounts

--- src/main/java/archives/tater/tooltrims/datagen/LootTableGenerator.java
+++ src/main/java/archives/tater/tooltrims/datagen/LootTableGenerator.java
@@ -1,7 +1,7 @@
 package archives.tater.tooltrims.datagen;
 
-import archives.tater.tooltrims.loot.ToolTrimsLoot;
 import archives.tater.tooltrims.item.ToolTrimsItems;
+import archives.tater.tooltrims.loot.ToolTrimsLoot;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
 import net.minecraft.item.Item;
@@ -13,7 +13,6 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.LootNumberProvider;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
@@ -25,7 +24,6 @@ public class LootTableGenerator extends SimpleFabricLootTableProvider {
         super(output, LootContextTypes.GENERIC);
     }
 
-    private static final LootNumberProvider ONE_OR_TWO = UniformLootNumberProvider.create(1, 2);
     private static final LootNumberProvider TWO = ConstantLootNumberProvider.create(2);
 
     private static LootTable.Builder singleton(Item item, @Nullable LootNumberProvider count) {
@@ -37,9 +35,9 @@ public class LootTableGenerator extends SimpleFabricLootTableProvider {
 
     @Override
     public void accept(BiConsumer<Identifier, LootTable.Builder> exporter) {
-        exporter.accept(ToolTrimsLoot.TRAIL_RUINS_INJECT, singleton(ToolTrimsItems.LINEAR_TOOL_TRIM_SMITHING_TEMPLATE, ONE_OR_TWO));
+        exporter.accept(ToolTrimsLoot.TRAIL_RUINS_INJECT, singleton(ToolTrimsItems.LINEAR_TOOL_TRIM_SMITHING_TEMPLATE, null));
         exporter.accept(ToolTrimsLoot.PILLAGER_OUTPOST_INJECT, singleton(ToolTrimsItems.TRACKS_TOOL_TRIM_SMITHING_TEMPLATE, TWO));
-        exporter.accept(ToolTrimsLoot.ANCIENCT_CITY_INJECT, singleton(ToolTrimsItems.CHARGE_TOOL_TRIM_SMITHING_TEMPLATE, ONE_OR_TWO));
+        exporter.accept(ToolTrimsLoot.ANCIENCT_CITY_INJECT, singleton(ToolTrimsItems.CHARGE_TOOL_TRIM_SMITHING_TEMPLATE, null));
         exporter.accept(ToolTrimsLoot.IGLOO_INJECT, LootTable.builder().pool(
                 LootPool.builder()
                     .with(ItemEntry.builder(ToolTrimsItems.FROST_TOOL_TRIM_SMITHING_TEMPLATE).weight(2))
