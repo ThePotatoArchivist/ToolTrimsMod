ba4036e24e7e2cde1ad05349ad32465f6d44c912
ThePotatoArchivist <josiahfu@gmail.com>
2026-06-09 02:11:04 -0700

    Better trimmable tools tag

--- src/client/java/archives/tater/tooltrims/datagen/ItemTagGenerator.java
+++ src/client/java/archives/tater/tooltrims/datagen/ItemTagGenerator.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 
 import java.util.concurrent.CompletableFuture;
@@ -18,46 +19,18 @@ public class ItemTagGenerator extends FabricTagsProvider.ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
-        valueLookupBuilder(ToolTrimsTags.TRIMMABLE_TOOLS).add(
-                Items.WOODEN_SWORD,
-                Items.WOODEN_SHOVEL,
-                Items.WOODEN_PICKAXE,
-                Items.WOODEN_AXE,
-                Items.WOODEN_HOE,
-                Items.STONE_SWORD,
-                Items.STONE_SHOVEL,
-                Items.STONE_PICKAXE,
-                Items.STONE_AXE,
-                Items.STONE_HOE,
-                Items.COPPER_SWORD,
-                Items.COPPER_SHOVEL,
-                Items.COPPER_PICKAXE,
-                Items.COPPER_AXE,
-                Items.COPPER_HOE,
-                Items.GOLDEN_SWORD,
-                Items.GOLDEN_SHOVEL,
-                Items.GOLDEN_PICKAXE,
-                Items.GOLDEN_AXE,
-                Items.GOLDEN_HOE,
-                Items.IRON_SWORD,
-                Items.IRON_SHOVEL,
-                Items.IRON_PICKAXE,
-                Items.IRON_AXE,
-                Items.IRON_HOE,
-                Items.DIAMOND_SWORD,
-                Items.DIAMOND_SHOVEL,
-                Items.DIAMOND_PICKAXE,
-                Items.DIAMOND_AXE,
-                Items.DIAMOND_HOE,
-                Items.NETHERITE_SWORD,
-                Items.NETHERITE_SHOVEL,
-                Items.NETHERITE_PICKAXE,
-                Items.NETHERITE_AXE,
-                Items.NETHERITE_HOE,
-                Items.BOW,
-                Items.CROSSBOW,
-                Items.TRIDENT,
-                Items.MACE
-        );
+        valueLookupBuilder(ToolTrimsTags.TRIMMABLE_TOOLS)
+                .forceAddTag(ItemTags.SWORDS)
+                .forceAddTag(ItemTags.PICKAXES)
+                .forceAddTag(ItemTags.AXES)
+                .forceAddTag(ItemTags.SHOVELS)
+                .forceAddTag(ItemTags.HOES)
+                .forceAddTag(ItemTags.SPEARS)
+                .add(
+                        Items.BOW,
+                        Items.CROSSBOW,
+                        Items.TRIDENT,
+                        Items.MACE
+                );
     }
 }
