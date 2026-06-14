0de2aeade4eaa4bcb554d65c7d4c2b8d496a3b08
ThePotatoArchivist <josiahfu@gmail.com>
2026-06-13 21:43:19 -0700

    End Reborn & Progression Reborn material compat

--- src/client/java/archives/tater/tooltrims/datagen/ClientTrimMaterialGenerator.java
+++ src/client/java/archives/tater/tooltrims/datagen/ClientTrimMaterialGenerator.java
@@ -4,7 +4,6 @@ import archives.tater.tooltrims.ToolTrims;
 import archives.tater.tooltrims.client.resource.ClientTrimMaterial;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricCodecDataProvider;
 
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.core.HolderLookup;
@@ -14,15 +13,39 @@ import net.minecraft.resources.Identifier;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
-public class ClientTrimMaterialGenerator extends FabricCodecDataProvider<ClientTrimMaterial> {
+import static net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions.allModsLoaded;
+
+public class ClientTrimMaterialGenerator extends ConditionalCodecDataProvider<ClientTrimMaterial> {
     protected ClientTrimMaterialGenerator(FabricPackOutput packOutput, CompletableFuture<HolderLookup.Provider> registriesFuture) {
         super(packOutput, registriesFuture, PackOutput.Target.RESOURCE_PACK, ClientTrimMaterial.Loader.PATH, ClientTrimMaterial.CODEC);
     }
 
     @Override
-    protected void configure(BiConsumer<Identifier, ClientTrimMaterial> provider, HolderLookup.Provider registryLookup) {
+    protected void configureC(BiConsumer<Identifier, ClientTrimMaterial> provider, HolderLookup.Provider registryLookup) {
         for (var trimMaterialModel : ItemModelGenerators.TRIM_MATERIAL_MODELS)
-            provider.accept(trimMaterialModel.materialKey().identifier(), new ClientTrimMaterial(ToolTrims.id("trims/color_palettes/" + trimMaterialModel.assets().base().suffix()), trimMaterialModel.assets().base().suffix()));
+            material(
+                    provider,
+                    trimMaterialModel.materialKey().identifier(),
+                    ToolTrims.id(trimMaterialModel.materialKey().identifier().getPath()),
+                    trimMaterialModel.assets().base().suffix()
+            );
+
+        materials(provider, "end_reborn", "crystalline", "featherzeal", "remnant");
+        materials(provider, "progression_reborn", "rose");
+    }
+
+    private void materials(BiConsumer<Identifier, ClientTrimMaterial> provider, String namespace, String... paths) {
+        var conditioned = withConditions(provider, allModsLoaded(namespace));
+        for (var path : paths)
+            material(conditioned, Identifier.fromNamespaceAndPath(namespace, path));
+    }
+
+    private static void material(BiConsumer<Identifier, ClientTrimMaterial> provider, Identifier id) {
+        material(provider, id, id, id.getNamespace() + "_" + id.getPath());
+    }
+
+    private static void material(BiConsumer<Identifier, ClientTrimMaterial> provider, Identifier id, Identifier texture, String suffix) {
+        provider.accept(id, new ClientTrimMaterial(texture.withPrefix("trims/color_palettes/"), suffix));
     }
 
     @Override
